32a2b48df5459b4f6187e0cc9d9a0cdee68d94d0
ludingsheng <1415731563@qq.com>
2023-04-06 15:03:18 +0800

    Desc： 读取文件

--- avg/avg.go
+++ avg/avg.go
@@ -2,8 +2,7 @@ package avg
 
 import "fmt"
 
-func GetAverage() {
-	numbers := [3]float64{56.1, 22.5, 36.2}
+func GetAverage(numbers [3]float64) {
 	var total float64 = 0
 	for _, number := range numbers {
 		total += number
